a31ca9fd6efa30b7aa42fd5db0e091a977cf447b
Junseok Oh <Junseok@Junseokui-MacBook-Pro.local>
2016-01-09 23:30:43 +0900

    Initial Layout Resources

---
+++ app/src/main/java/kr/edcan/exchat/MainActivity.java
@@ -0,0 +1,69 @@
+package kr.edcan.exchat;
+
+import android.content.res.Configuration;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+    ActionBarDrawerToggle toggle;
+    DrawerLayout drawerLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setDefault();
+        setSupportActionBar();
+    }
+
+    private void setDefault() {
+    }
+
+    private void setSupportActionBar() {
+        drawerLayout = (DrawerLayout)findViewById(R.id.drawer);
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toggle = new ActionBarDrawerToggle(this,
+                drawerLayout, R.string.app_name, R.string.app_name);
+        drawerLayout.setDrawerListener(toggle);
+
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+
+// Sync the toggle state after onRestoreInstanceState has occurred.
+        toggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        toggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (toggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
